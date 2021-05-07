95ad50d6e5b4799f070342c7e37a5ac647b1356f
KING PENSON <kjpenson2@gmail.com>
2021-05-07 18:23:25 -0500

    Added schemas for product, comments, alertcount

--- routes/index.js
+++ routes/index.js
@@ -2,37 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb+srv://kingpen2:Kestrel24!@cluster0.br9xr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
- db.once('open', function() {
-   // we're connected!
-   console.log("Connected to DB")
+
+
+//Product, Comment, Alert Schemas
+const commentSchema = new mongoose.Schema({
+    content: String, 
+    timestamp: String, 
+    type: Date
  });
 
-//Product Schema
+const alertSchema = new mongoose.Schema({
+    alertCountWeekly: Number, 
+    alertCountDaily: Number, 
+    timestamp: Date
+});
+
 const productSchema = new mongoose.Schema({
     orgName: String,
     endpointCount: Number,
     endpointDeployed: Number,
     product: String,
     status: String,
-    //Add array properties below
+    alertCounts: [alertSchema],
+    comments: [commentSchema]
 })
 
 const Product = mongoose.model('Product', productSchema);
-const orgOne = new Product({ orgName: 'orgOne', endpointCount: 2000, endpointDeployed: 244, product: "XDR", status: "Tuning" });
-console.log(orgOne);
 
-orgOne.save(function (err, orgOne) {
-    if (err) return console.error(err);
-});
 
-Product.find(function (err, products) {
-if (err) return console.error(err);
-console.log(products);
-})
+// orgOne.save(function (err, orgOne) {
+//     if (err) return console.error(err);
+// });
+
+let allProducts = function(){
+    
+}
 
 
 let orgProductList = [];
@@ -40,22 +46,19 @@ let orgProductList = [];
 let createProduct = function(orgName, endpointCount, endpointDeployed, product){
     let status = ["hold", "tuning", "production","playbook"]
     let orgProduct = {};
+
     
-    //Name of org, product, endpointCount
+    //Name of org, product, endpointCount, Status
     orgProduct["orgName"] = orgName
     orgProduct["endpointCount"] = endpointCount
     orgProduct["endpointDeployed"] = endpointDeployed
     orgProduct["product"] = product
-    
-    //Status
     orgProduct["status"] = status[1];
     
-    //Alert Count
+    //Alert Count & Log
     alertCountWeekly = 100
     alertCountDaily = 24
     let alertTimestamp = "3-10-2021"
-    
-    //Log of alert counts
     alertCounts = [
         {
         "alertCountWeekly":alertCountWeekly, 
@@ -88,7 +91,6 @@ let createProduct = function(orgName, endpointCount, endpointDeployed, product){
             group["exploit"] = exploit
             group["malware"] = malware
             groups.push(group)
-
         }
         let addNewGroup = true
         if(addNewGroup == true){
@@ -96,25 +98,33 @@ let createProduct = function(orgName, endpointCount, endpointDeployed, product){
         } else{
             console.log("No more groups to be added")
         }
-
         orgProduct["groups"] = groups  
 
+        //Push to Product List Array
         orgProductList.push(orgProduct)
+
+        //Define product model
+        const newOrg = new Product(
+            { 
+                orgName: orgName, 
+                endpointCount: endpointCount, 
+                endpointDeployed: endpointDeployed, 
+                product: product, 
+                status: status
+            }
+        );
+        console.log(newOrg)
     }
-
-    
-    
-
 }
 
 createProduct("organization1",2000,1025,"XDR");
-createProduct("organization2",500,25,"XDR");
+// createProduct("organization2",500,25,"XDR");
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express'});
-  res.send(orgProductList)
+  res.send(allProducts)
 });
 
 module.exports = router;

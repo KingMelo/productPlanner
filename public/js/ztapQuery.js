991f8a9dc9dc37c1ed9f4f783edfcb9c6a3998c4
KING PENSON <kjpenson2@gmail.com>
2021-05-20 18:47:22 -0500

    Added ztapquery file for querying entries

---
+++ public/js/ztapQuery.js
@@ -0,0 +1,22 @@
+//Alert Queries
+
+//Last Week: https://portalapi.threatanalytics.io/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1week&Organization=Bregal%20Investments%2FSagemount&Product=devo&assigned%20escalation%20path=product%20tuning%20%28criticalstart%20mdr%29&fields=32&incident%20status=Open
+let getWeeklyAlert = function(total){
+    
+}
+
+//Last Day: https://portalapi.threatanalytics.io/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1day&Organization=Bregal%20Investments%2FSagemount&Product=devo&assigned%20escalation%20path=product%20tuning%20%28criticalstart%20mdr%29&fields=32&incident%20status=Open
+let getDailyAlert = function(total){
+    
+}
+
+//Filter: https://portalapi.threatanalytics.io/api/1.5/matchers/?limit=10&ordering=created%3Adesc&page=1&product=devo&psa_id=bregal&typ=filter
+let getFilters = function(prodId){
+    
+}
+
+//Get Product: https://portalapi.threatanalytics.io/api/1.5/subscriptions/1633/
+//Check if parent: https://portalapi.threatanalytics.io/api/1.5/organizations/966/ (parent key)
+let getProduct = function(){
+
+}

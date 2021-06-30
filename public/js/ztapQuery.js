64393fcab6512ba382155911e6a0eaf071350029
KING PENSON <kjpenson2@gmail.com>
2021-06-29 19:19:47 -0500

    Added daily alert function and checked if it worked for all orgs and products

--- public/js/ztapQuery.js
+++ public/js/ztapQuery.js
@@ -6,7 +6,7 @@ let apiAuth = require("../../apiKey")
 let weeklyAlert, dailyAlert, filters, product;
 
 const options = {
-    hostname: 'portalapi.threatanalytics.io',
+    hostname: 'https://portalapi.threatanalytics.io',
     port: 443,
     path: '/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1week&Incident%20Status=Open&Organization=poolcorp&Product=azure_sentinel&fields=32',
     method: 'GET',
@@ -17,34 +17,50 @@ const options = {
   };
  
 
-//Last Week https://portalapi.threatanalytics.io/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1week&Incident%20Status=Open&Organization=poolcorp&Product=azure_sentinel&fields=32
-//Need to get "total" key for alert count
-let getWeeklyAlert = function(){
-    axios.get('https://portalapi.threatanalytics.io/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1week&Incident%20Status=Open&Organization=poolcorp&Product=azure_sentinel&fields=32', {
+let getWeeklyAlert = function(org, product){
+    //Get query parameters
+    let queryString = options.hostname + '/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1week&Incident%20Status=Open&Organization=' + org + "&Product=" + product + "&fields=32"
+    
+    axios.get(queryString, {
         headers: {
             'authorization': apiAuth.key,
             'x-organization': apiAuth.xOrg
         }
     })
     .then(result => {
-        console.log(result.data.total)
+        console.log(result.data.total + " " + "alerts in the last 7 days")
     })
     .catch(error => {
         console.log(error)
     })
     
 }
-getWeeklyAlert();
+getWeeklyAlert("ahernrentals", "xdr");
 
 
 
 
 
 
-//Last Day: https://portalapi.threatanalytics.io/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1day&Organization=Bregal%20Investments%2FSagemount&Product=devo&assigned%20escalation%20path=product%20tuning%20%28criticalstart%20mdr%29&fields=32&incident%20status=Open
-let getDailyAlert = function(total){
+let getDailyAlert = function(org, product){
+    //Get query parameters
+    let queryString = options.hostname + '/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1day&Incident%20Status=Open&Organization=' + org + "&Product=" + product + "&fields=32"
+    
+    axios.get(queryString, {
+        headers: {
+            'authorization': apiAuth.key,
+            'x-organization': apiAuth.xOrg
+        }
+    })
+    .then(result => {
+        console.log(result.data.total + " " + "alerts in the last 24 hours")
+    })
+    .catch(error => {
+        console.log(error)
+    })
     
 }
+getDailyAlert("ahernrentals", "xdr");
 
 //Filter: https://portalapi.threatanalytics.io/api/1.5/matchers/?limit=10&ordering=created%3Adesc&page=1&product=devo&psa_id=bregal&typ=filter
 let getFilters = function(prodId){

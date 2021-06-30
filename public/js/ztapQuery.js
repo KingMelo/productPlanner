611d31382ebade7861099a8ca258fdfbc0dbc460
KING PENSON <kjpenson2@gmail.com>
2021-06-30 15:58:34 -0500

    Added getParent function

--- public/js/ztapQuery.js
+++ public/js/ztapQuery.js
@@ -3,7 +3,6 @@ const { default: axios } = require('axios');
 const https = require('axios');
 let apiAuth = require("../../apiKey")
 
-let weeklyAlert, dailyAlert, filters, product;
 
 const options = {
     hostname: 'https://portalapi.threatanalytics.io',
@@ -13,8 +12,7 @@ const options = {
         'authorization': apiAuth.key,
         'x-organization': apiAuth.xOrg
     }
-  };
- 
+  }; 
 
 let getWeeklyAlert = function(org, product){
     //Get query parameters
@@ -34,12 +32,6 @@ let getWeeklyAlert = function(org, product){
     })
     
 }
-getWeeklyAlert("ahernrentals", "xdr");
-
-
-
-
-
 
 let getDailyAlert = function(org, product){
     //Get query parameters
@@ -56,19 +48,30 @@ let getDailyAlert = function(org, product){
     })
     .catch(error => {
         console.log(error)
-    })
-    
+    })    
 }
 
-
-
-
-//Get Product: https://portalapi.threatanalytics.io/api/1.5/subscriptions/1633/
-//Check if parent: https://portalapi.threatanalytics.io/api/1.5/organizations/966/ (parent key)
-let getProduct = function(productNumber){
+// //Get parent of org product
+// let getDailyAlert = function(org, product){
+//     //Get query parameters
+//     let queryString = options.hostname + '/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1day&Incident%20Status=Open&Organization=' + org + "&Product=" + product + "&fields=32"
     
-    //Get psaid and name
+//     axios.get(queryString, {
+//         headers: {
+//             'authorization': apiAuth.key,
+//             'x-organization': apiAuth.xOrg
+//         }
+//     })
+//     .then(result => {
+//         console.log(result.data.total + " " + "alerts in the last 24 hours")
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })    
+// }
 
+
+let getProduct = function(productNumber){
     //Get product status
     let queryString = options.hostname + '/api/1.5/subscriptions/' + productNumber + '/'
     axios.get(queryString, {
@@ -78,26 +81,42 @@ let getProduct = function(productNumber){
         }
     })
     .then(result => {
-        console.log(result.data)
-        console.log("Org Name:" + " " + result.data.organization.name)
-        console.log("Psaid:" + " " + result.data.organization.psa_id)
-        console.log("Product Identifier:" + " " + result.data.name)
-        console.log("Product Name:" + " " + result.data.name_label)
-        console.log("ZTAP ID:" + " " + result.data.id)
-        console.log("Deployment Status:" + " " + result.data.status)
-        console.log("Install Date:" + " " + result.data.install_date)
-        console.log("Purchased Volume:" + " " + result.data.license.purchased)
-        console.log("Deployed:" + " " + result.data.license.verified)    
+        //console.log(result.data)
+        console.log("Org Name: " + result.data.organization.name)
+        console.log("Org Id: " + result.data.organization.id)
+        console.log("Psaid: " + result.data.organization.psa_id)
+        console.log("Product Identifier: " + result.data.name)
+        console.log("Product Name: " + result.data.name_label)
+        console.log("ZTAP ID: " + result.data.id)
+        console.log("Deployment Status: " + result.data.status)
+        console.log("Install Date: " + result.data.install_date)
+        console.log("Purchased Volume: " + result.data.license.purchased)
+        console.log("Deployed: " + result.data.license.verified)    
         
         getDailyAlert(result.data.organization.psa_id, result.data.name)
+        getWeeklyAlert(result.data.organization.psa_id, result.data.name)
+        
+        //Get Parent
+        function getParent(orgId){
+            let queryString = options.hostname + '/api/1.5/organizations/' + orgId + '/'
+            axios.get(queryString, {
+                headers: {
+                    'authorization': apiAuth.key,
+                    'x-organization': apiAuth.xOrg
+                }
+            }).then(result => {
+                console.log("Parent Name: " + result.data.parent.name)
+                console.log("Parent psaid: " + result.data.parent.psa_id)                
+            })
+        } getParent(result.data.organization.id)
+
     })
     .catch(error => {
         console.log(error)
-    }) 
+    })  
 
-    
-    
+
 }
-getProduct(1220);
+//getProduct(2195);
 
-module.exports = getWeeklyAlert;
+module.exports = getWeeklyAlert, getProduct;

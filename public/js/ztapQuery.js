70e742ca7fbd69377c754a7f0ed308ebed8f7326
KING PENSON <kjpenson2@gmail.com>
2021-07-02 15:43:07 -0500

    fixed axios get request, can now call to index.js

--- public/js/ztapQuery.js
+++ public/js/ztapQuery.js
@@ -1,7 +1,10 @@
 //Alert Queries
 const { default: axios } = require('axios');
-const https = require('axios');
+
+const { response } = require('express');
 let apiAuth = require("../../apiKey")
+
+
 
 
 const options = {
@@ -14,6 +17,50 @@ const options = {
     }
   }; 
 
+  async function getProduct(productNumber){
+    
+  
+    let queryString = options.hostname + '/api/1.5/subscriptions/' + productNumber + '/'
+    const promise = axios.get(queryString, {
+        headers: {
+            'authorization': apiAuth.key,
+            'x-organization': apiAuth.xOrg
+        }
+    })
+
+    const dataPromise = promise.then((response) => response.data)
+
+    return dataPromise
+
+}
+   
+
+
+
+
+
+
+
+
+
+//Get Parent
+function getParent(orgId){
+    let queryString = options.hostname + '/api/1.5/organizations/' + orgId + '/'
+    axios.get(queryString, {
+        headers: {
+            'authorization': apiAuth.key,
+            'x-organization': apiAuth.xOrg
+        }
+    }).then(result => {
+        obj['parentName'] =  data.parent.name
+        obj['parentId'] =  data.parent.psa_id
+        // console.log("Parent Name: " + parentName)
+        // console.log("Parent psaid: " + parentNameId)      
+        
+    })
+} 
+
+
 let getWeeklyAlert = function(org, product){
     //Get query parameters
     let queryString = options.hostname + '/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1week&Incident%20Status=Open&Organization=' + org + "&Product=" + product + "&fields=32"
@@ -25,12 +72,12 @@ let getWeeklyAlert = function(org, product){
         }
     })
     .then(result => {
-        console.log(result.data.total + " " + "alerts in the last 7 days")
+        
+        console.log( data.total + " " + "alerts in the last 7 days")
     })
     .catch(error => {
         console.log(error)
-    })
-    
+    })    
 }
 
 let getDailyAlert = function(org, product){
@@ -44,79 +91,20 @@ let getDailyAlert = function(org, product){
         }
     })
     .then(result => {
-        console.log(result.data.total + " " + "alerts in the last 24 hours")
+        obj['dailyAlerts'] =  data.total
+        console.log( data.total + " " + "alerts in the last 24 hours")
     })
     .catch(error => {
         console.log(error)
     })    
 }
 
-// //Get parent of org product
-// let getDailyAlert = function(org, product){
-//     //Get query parameters
-//     let queryString = options.hostname + '/api/1.5/incidents/?-Sort%20By=Incident%20Created&Incident%20Created=1day&Incident%20Status=Open&Organization=' + org + "&Product=" + product + "&fields=32"
-    
-//     axios.get(queryString, {
-//         headers: {
-//             'authorization': apiAuth.key,
-//             'x-organization': apiAuth.xOrg
-//         }
-//     })
-//     .then(result => {
-//         console.log(result.data.total + " " + "alerts in the last 24 hours")
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     })    
-// }
 
 
-let getProduct = function(productNumber){
-    //Get product status
-    let queryString = options.hostname + '/api/1.5/subscriptions/' + productNumber + '/'
-    axios.get(queryString, {
-        headers: {
-            'authorization': apiAuth.key,
-            'x-organization': apiAuth.xOrg
-        }
-    })
-    .then(result => {
-        //console.log(result.data)
-        console.log("Org Name: " + result.data.organization.name)
-        console.log("Org Id: " + result.data.organization.id)
-        console.log("Psaid: " + result.data.organization.psa_id)
-        console.log("Product Identifier: " + result.data.name)
-        console.log("Product Name: " + result.data.name_label)
-        console.log("ZTAP ID: " + result.data.id)
-        console.log("Deployment Status: " + result.data.status)
-        console.log("Install Date: " + result.data.install_date)
-        console.log("Purchased Volume: " + result.data.license.purchased)
-        console.log("Deployed: " + result.data.license.verified)    
-        
-        getDailyAlert(result.data.organization.psa_id, result.data.name)
-        getWeeklyAlert(result.data.organization.psa_id, result.data.name)
-        
-        //Get Parent
-        function getParent(orgId){
-            let queryString = options.hostname + '/api/1.5/organizations/' + orgId + '/'
-            axios.get(queryString, {
-                headers: {
-                    'authorization': apiAuth.key,
-                    'x-organization': apiAuth.xOrg
-                }
-            }).then(result => {
-                console.log("Parent Name: " + result.data.parent.name)
-                console.log("Parent psaid: " + result.data.parent.psa_id)                
-            })
-        } getParent(result.data.organization.id)
-
-    })
-    .catch(error => {
-        console.log(error)
-    })  
 
 
-}
-//getProduct(2195);
 
-module.exports = getWeeklyAlert, getProduct;
+
+exports.getProduct = getProduct;
+//exports.getDailyAlert = getDailyAlert;
+//exports.getWeeklyAlert = getWeeklyAlert;
--- routes/index.js
+++ routes/index.js
@@ -41,43 +41,35 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 
-//Get Organization with Product
-let createProduct = function(orgName, endpointCount, endpointDeployed, product, status){
-    let orgProduct = {};
-    
+ztap.getProduct(1409)
+    .then(data => {
+        let obj = {}
 
-    
-    //Name of org, product, endpointCount, Status
-    orgProduct["orgName"] = orgName
-    orgProduct["endpointCount"] = endpointCount
-    orgProduct["endpointDeployed"] = endpointDeployed
-    orgProduct["product"] = product
-    orgProduct["status"] = status;
-    
-    //Alert Count & Log
-    alertCounts = []
-    orgProduct["alertCounts"] = alertCounts
+        obj['orgName'] =  data.organization.name
+        obj['orgId'] =  data.organization.id
+        obj['psaId'] =  data.organization.psa_id
+        obj['productId'] =  data.name
+        obj['productName'] =  data.name_label
+        obj['status'] = data.status
+        obj['ztapId'] =  data.id
+        obj['status'] =  data.status
+        obj['installDate'] =  data.install_date
+        obj['licenseCount'] =  data.license.purchased
+        obj['deployed'] =  data.license.verified
 
-    //Add comment section
-    let comments = []
-    orgProduct["comments"] = comments;
+        console.log(obj)
 
-    //If XDR product, add policy/group name 
-    let groups = []
-    orgProduct["groups"] = groups  
-
-
-        //Define product model
+        //Save Product to db
         const newOrg = new Product(
             { 
-                orgName: orgName, 
-                endpointCount: endpointCount, 
-                endpointDeployed: endpointDeployed, 
-                product: product, 
-                status: status,
-                groups: groups,
-                alertCounts: alertCounts,
-                comments: comments
+                orgName: obj.orgName, 
+                // endpointCount: endpointCount, 
+                // endpointDeployed: endpointDeployed, 
+                // product: product, 
+                // status: status,
+                // groups: groups,
+                // alertCounts: alertCounts,
+                // comments: comments
             }
         );
 
@@ -85,19 +77,57 @@ let createProduct = function(orgName, endpointCount, endpointDeployed, product, 
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function() {
             console.log("Connected to DB")
-            console.log("Adding new org " + orgName)
+            console.log("Adding new org " + newOrg.orgName)
             
-            newOrg.save(function(err, newOrg) {
-                if (err) return console.error(err);
-            })
+            // newOrg.save(function(err, newOrg) {
+            //     if (err) return console.error(err);
+            // })
         });
 
-        
-    }
+    })
+    .catch(err => console.log(err))
 
 
-//createProduct("organization 3",2000,1025,"XDR","tuning");
-// createProduct("organization2",500,25,"XDR");
+let createProduct = function(orgName, licenseCount, deployed, productName, status){
+    let orgProduct = {};
+    
+    
+    orgProduct["orgName"] = orgName
+    // orgProduct["endpointCount"] = endpointCount
+    // orgProduct["endpointDeployed"] = endpointDeployed
+    // orgProduct["product"] = product
+    // orgProduct["status"] = status;
+    
+    //Alert Count & Log
+    // alertCounts = []
+    // orgProduct["alertCounts"] = alertCounts
+
+    // //Add comment section
+    // let comments = []
+    // orgProduct["comments"] = comments;
+
+    // //If XDR product, add policy/group name 
+    // let groups = []
+    // orgProduct["groups"] = groups  
+
+    //Define product model
+    const newOrg = new Product(
+        { 
+            orgName: orgName, 
+            // endpointCount: endpointCount, 
+            // endpointDeployed: endpointDeployed, 
+            // product: product, 
+            // status: status,
+            // groups: groups,
+            // alertCounts: alertCounts,
+            // comments: comments
+        }
+    );
+
+    
+
+    
+}
 
 
 /* GET home page. */
@@ -110,23 +140,7 @@ router.get('/', function(req, res, next) {
       
         });
     })
-
-    
 });
-
-// Parameter for searched
-// router.get('/poop', function(req, res, next) {
-//     Product.find({}, function(err, products){
-//         res.render('poop', { 
-//             title: 'Espresso', 
-//             products: products,
-      
-//         });
-//     })
-// });
-
-
-
 
 
 module.exports = router;
